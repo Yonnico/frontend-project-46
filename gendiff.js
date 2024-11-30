bdb5a1e22a84995fa090e652af589d6140414928
Yonnico <yonyonico@gmail.com>
2024-12-01 00:56:41 +0500

    step 2: update gendiff.js, add arguments and format option

--- gendiff.js
+++ gendiff.js
@@ -4,5 +4,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and generates a difference report.')
   .version('0.0.1')
+  .argument('<filipath1>', 'first file path')
+  .argument('<filipath2>', 'second file path')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
