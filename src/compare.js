0391454fe4f372251d0e680b82836c793cd22bd9
Yonnico <yonyonico@gmail.com>
2025-01-26 23:00:25 +0500

    refactor

--- src/compare.js
+++ src/compare.js
@@ -23,61 +23,49 @@ import _ from 'lodash';
  * @returns {Difference[]} - Массив различий между объектами
  */
 export const compare = (object1, object2, parentPath = []) => {
-
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
-
   const keys = _.union(keys1, keys2).toSorted();
 
   const buildDifference = (key) => {
-
     const value1 = object1[key];
     const value2 = object2[key];
     const currentPath = [...parentPath, key];
     const keyPath = currentPath.join('.');
 
+    /**
+     * @param {string} typeDifference - Тип различия между объектами
+     * @param {unknown} oldValue - Значение в первом объекте
+     * @param {unknown} newValue - Значение во втором объекте 
+     * @param {?Difference[]} children - Дочерние элементы
+     * @returns {Difference} - Объект с информацией о различии
+     */
+    const createDiff = (typeDifference, oldValue, newValue, children = null) => ({
+      key,
+      keyPath,
+      typeDifference,
+      oldValue,
+      newValue,
+      children,
+    });
+
     if (key in object1 && !(key in object2)) {
-      return {
-        key,
-        keyPath,
-        typeDifference: 'deleted',
-        oldValue: value1,
-        newValue: null,
-        children: null,
-      };
+      return createDiff('deleted', value1, null);
     }
 
     if (!(key in object1) && key in object2) {
-      return {
-        key,
-        keyPath,
-        typeDifference: 'added',
-        oldValue: null,
-        newValue: value2,
-        children: null,
-      };
+      return createDiff('added', null, value2);
     }
 
     const isEqual = _.isEqual(value1, value2);
     const isBothObjects = _.isObject(value1) && _.isObject(value2);
 
-    const result = {
-      key,
-      keyPath,
-      typeDifference: isEqual ? 'unchanged' : 'changed',
-      oldValue: value1,
-      newValue: value2,
-      children: null,
-    };
-
     if (!isEqual && isBothObjects) {
-      result.children = compare(value1, value2, currentPath);
+      return createDiff('changed', value1, value2, compare(value1, value2, currentPath));
     }
 
-    return result;
+    return createDiff(isEqual ? 'unchanged' : 'changed', value1, value2);
   };
 
-  const difference = keys.map(buildDifference);
-
-  return difference;
+  return keys.map(buildDifference);
 };
