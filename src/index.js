59aa8738e4b535c20b832acbe7a85847c78e30ed
Yonnico <yonyonico@gmail.com>
2025-01-26 19:48:04 +0500

    Добавил документацию

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { fileParser } from "./parsers.js";
 import { compare } from "./compare.js";
-import { formatter } from "./formatter.js";
+import { formatter } from "./formatters/index.js";
 
+/**
+ * @param {string} filepath1 - Путь к первому файлу
+ * @param {string} filepath2 - Путь ко второму файлу
+ * @param {string} format - Формат вывода (по умолчанию "stylish")
+ * @returns {string} - Строка с результатом сравнения
+ */
 export const gendiff = (filepath1, filepath2, format = "stylish") => {
 
   const file1 = fileParser(filepath1);
--- src/parsers.js
+++ src/parsers.js
@@ -3,20 +3,33 @@ import path from 'path';
 import process from 'process';
 import yaml from 'js-yaml';
 
+/**
+ * @param {string} filepath - Путь к файлу
+ * @returns {string} - Абсолютный путь к файлу
+ */
 export const getPath = (filepath) => path.resolve(process.cwd(), filepath);
+
+/**
+ * @param {string} filepath - Путь к файлу
+ * @returns {string} - Содержимое файла
+ */
 export const readFile = (filepath) => fs.readFileSync(getPath(filepath), 'utf-8');
 
+/**
+ * @param {string} filepath - Путь к файлу
+ * @returns {object} - Содержимое файла
+ */
 export const fileParser = (filepath) => {
 
   const parseTypes = {
-    json: JSON.parse,
-    yml: yaml.load,
-    yaml: yaml.load
+    json: (content) => JSON.parse(content),
+    yml: (content) => yaml.load(content),
+    yaml: (content) => yaml.load(content)
   };
 
   const content = readFile(filepath);
   const type = path.extname(filepath).slice(1);
-  
+
   const result = parseTypes[type](content);
 
   return result;
