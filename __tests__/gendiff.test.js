24f9f9cdab370f2866d00984d81d2cca7475656e
Yonnico <yonyonico@gmail.com>
2025-01-27 22:23:28 +0500

    fix lint

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,29 +2,25 @@ import { describe, test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
 import { readFile } from '../src/parsers.js';
 
-const formats = ['json', 'yaml', 'yml'];
-const formatters = {
-  stylish: { formatter: 'stylish', expected: '__fixtures__/stylishExpected.txt' },
-  plain: { formatter: 'plain', expected: '__fixtures__/plainExpected.txt' },
-  json: { formatter: 'json', expected: '__fixtures__/jsonExpected.json' }
-};
-
 describe('Сравнение файлов', () => {
-  test.each(Object.entries(formatters))(
-    'Формат вывода: %s',
-    (formatterName, { formatter, expected: expectedPath }) => {
-      test.each(formats)(
-        'Сравнение %s файлов',
-        (fileFormat) => {
-          const file1 = `__fixtures__/test1.${fileFormat}`;
-          const file2 = `__fixtures__/test2.${fileFormat}`;
+  const formats = ['json', 'yaml', 'yml'];
+  const expectedFiles = {
+    stylish: '__fixtures__/stylishExpected.txt',
+    plain: '__fixtures__/plainExpected.txt',
+    json: '__fixtures__/jsonExpected.json'
+  };
 
-          const actual = gendiff(file1, file2, formatter);
-          const expected = readFile(expectedPath);
-
-          expect(actual).toEqual(expected);
-        }
-      );
-    }
+  const testCases = formats.flatMap((format) =>
+    ['stylish', 'plain', 'json'].map((formatter) => [format, formatter])
   );
+
+  test.each(testCases)('Сравнение %s файлов с форматом вывода %s', (format, formatter) => {
+    const file1 = `__fixtures__/test1.${format}`;
+    const file2 = `__fixtures__/test2.${format}`;
+
+    const actual = gendiff(file1, file2, formatter);
+    const expected = readFile(expectedFiles[formatter]);
+
+    expect(actual).toEqual(expected);
+  });
 });
